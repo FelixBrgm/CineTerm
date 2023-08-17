0d5eec8f13aa58846c1d3ed2a36f665b14e50318
Felix Brüggemann <Felix.brgm@icloud.com>
2023-08-17 13:07:33 +0200

    Added descriptions

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use std::io::Read;
 use std::thread;
 use std::time;
 
+/// A struct representing a frame with pixels arranged in a grid.
 pub struct Frame {
+    /// The height of the frame (number of rows).
     height: usize,
+    /// The width of the frame (number of columns).
     width: usize,
+    /// A 2D vector containing the pixels of the frame.
     pixels: Vec<Vec<char>>,
 }
 
@@ -24,8 +28,21 @@ impl Clone for Frame {
     }
 }
 
+#[allow(dead_code)]
 impl Frame {
-    // Constructors
+    /// Creates a new `Frame` instance with the specified dimensions.
+    ///
+    /// This function initializes a new `Frame` instance with the given height and width.
+    /// Each pixel in the frame is initialized to a space character (' ').
+    ///
+    /// # Arguments
+    ///
+    /// * `height` - The height (number of rows) of the frame.
+    /// * `width` - The width (number of columns) of the frame.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new `Frame` instance with the specified dimensions and all pixels set to spaces.
     pub fn new(height: usize, width: usize) -> Self {
         Frame {
             pixels: vec![vec![' '; width]; height],
@@ -34,14 +51,43 @@ impl Frame {
         }
     }
 
+    /// Creates a new `Frame` instance from a string.
+    ///
+    /// This function takes a reference to a string and creates a new `Frame` instance
+    /// with a single row and a width equal to the length of the input string.
+    /// The contents of the string are used to populate the row of pixels in the frame.
+    ///
+    /// # Arguments
+    ///
+    /// * `str` - A reference to a string containing the pixel data for the frame.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new `Frame` instance with a single row of pixels populated from the input string.
     pub fn from_string(str: &str) -> Self {
         let mut frame = Frame::new(1, str.len());
 
-        frame.override_with_string(str);
+        frame.overlay_with_string(str);
 
         frame
     }
 
+    /// Creates a new `Frame` instance from a file.
+    ///
+    /// This function reads pixel data from a file specified by the `file_path` argument.
+    /// The file is expected to contain rows of characters, each representing a row of pixels.
+    /// The number of rows and width of the frame are determined by the `height` and `width` arguments.
+    ///
+    /// # Arguments
+    ///
+    /// * `height` - The height (number of rows) of the frame.
+    /// * `width` - The width (number of columns) of the frame.
+    /// * `file_path` - The path to the file containing pixel data.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` where `Ok` contains a new `Frame` instance populated with pixel data from the file,
+    /// or an `Err` if there was an error reading the file or populating the frame.
     pub fn from_file(height: usize, width: usize, file_path: &str) -> Result<Self, std::io::Error> {
         let mut file = File::open(file_path)?;
         let mut file_contents = String::new();
@@ -52,18 +98,39 @@ impl Frame {
         let mut frame = Frame::new(height, width);
 
         for (index, line) in lines.iter().enumerate() {
-            frame.override_with_string_at_position(0, index as i32, line);
+            frame.overlay_with_string_at_position(0, index as i32, line);
         }
 
         Ok(frame)
     }
 
-    // Override
-    pub fn override_with_frame(&mut self, frame: &Frame) {
-        self.override_with_frame_at_position(0, 0, frame);
+    /// overlays the pixel data of the current frame with the pixel data from another frame.
+    ///
+    /// This function takes a reference to another `Frame` instance and replaces the pixel data in the current frame
+    /// with the pixel data from the provided frame. The operation starts at the top-left corner of the current frame.
+    ///
+    /// # Arguments
+    ///
+    /// * `frame` - A reference to another `Frame` instance whose pixel data will overlay the current frame's data.
+    pub fn overlay_with_frame(&mut self, frame: &Frame) {
+        self.overlay_with_frame_at_position(0, 0, frame);
     }
 
-    pub fn override_with_frame_at_position(&mut self, x: i32, y: i32, frame: &Frame) {
+    /// overlays a portion of the current frame with pixel data from another frame.
+    ///
+    /// This function takes a reference to another `Frame` instance and replaces a portion of the pixel data
+    /// in the current frame with the corresponding portion from the provided frame. The starting position
+    /// for the overlay is specified by the `x` and `y` parameters.
+    ///
+    /// If the starting position (x, y) is outside the bounds of the current frame or the provided frame,
+    /// the function will skip those pixels.
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - The X-coordinate of the starting position for the overlay.
+    /// * `y` - The Y-coordinate of the starting position for the overlay.
+    /// * `frame` - A reference to another `Frame` instance whose pixel data will overlay a portion of the current frame's data.
+    pub fn overlay_with_frame_at_position(&mut self, x: i32, y: i32, frame: &Frame) {
         for (line_index, line) in frame.pixels.iter().enumerate() {
             if line_index as i32 + y > self.height as i32 - 1 || line_index as i32 + y < 0 {
                 continue;
@@ -77,20 +144,63 @@ impl Frame {
         }
     }
 
-    pub fn override_with_string(&mut self, str: &str) {
-        self.override_with_string_at_position(0, 0, str);
+    /// overlays the pixel data of the current frame with characters from a string.
+    ///
+    /// This function takes a reference to a string and replaces a portion of the pixel data in the current frame
+    /// with characters from the input string. The operation starts at the top-left corner of the current frame.
+    ///
+    /// # Arguments
+    ///
+    /// * `str` - A reference to a string containing the new pixel data for the frame.
+    pub fn overlay_with_string(&mut self, str: &str) {
+        self.overlay_with_string_at_position(0, 0, str);
     }
 
-    pub fn override_with_string_at_position(&mut self, x: i32, y: i32, str: &str) {
-        let mut frame: Frame = Frame::new(1, str.len());
-
+    /// overlays a portion of the current frame with characters from a string at a specific position.
+    ///
+    /// This function takes a reference to a string and replaces a portion of the pixel data in the current frame
+    /// with characters from the input string. The starting position for the overlay is specified by the `x` and `y` parameters.
+    ///
+    /// If the starting position is outside the width bounds of the frame, the function will only overlay the pixels
+    /// that fit within the width of the frame.
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - The X-coordinate of the starting position for the overlay.
+    /// * `y` - The Y-coordinate of the starting position for the overlay.
+    pub fn overlay_with_string_at_position(&mut self, x: i32, y: i32, str: &str) {
         for (c_index, c) in str.as_bytes().iter().enumerate() {
-            frame.pixels[0][c_index] = *c as char;
+            if c_index < self.width {
+                self.pixels[0][c_index] = *c as char;
+            }
         }
-
-        self.override_with_frame_at_position(x, y, &frame);
     }
 
+    /// Gets the width of the current frame.
+    ///
+    /// This function returns the width (number of columns) of the current frame.
+    ///
+    /// # Returns
+    ///
+    /// Returns the width of the frame.
+    fn get_width(&self) -> usize {
+        self.width
+    }
+
+    /// Gets the height of the current frame.
+    ///
+    /// This function returns the height (number of rows) of the current frame.
+    ///
+    /// # Returns
+    ///
+    /// Returns the height of the frame.
+    fn get_height(&self) -> usize {
+        self.height
+    }
+
+    /// Prints the contents of the current frame.
+    ///
+    /// This function prints the pixel data of the current frame to the console, row by row.
     pub fn print(&self) {
         for line in &self.pixels {
             for c in line {
@@ -99,25 +209,32 @@ impl Frame {
             print!("\n");
         }
     }
-
-    fn get_width(&self) -> usize {
-        self.width
-    }
-
-    fn get_height(&self) -> usize {
-        self.height
-    }
 }
 
+/// A struct representing a movie composed of frames.
 pub struct Movie {
+    /// The width of each frame in the movie.
     width: usize,
+    /// The height of each frame in the movie.
     height: usize,
+    /// The collection of frames that make up the movie.
     frames: Vec<Frame>,
 }
 
 #[allow(dead_code)]
 impl Movie {
-    // Constructors
+    /// Creates a new empty `Movie` instance with the specified dimensions.
+    ///
+    /// This function initializes a new `Movie` instance with the given height and width.
+    ///
+    /// # Arguments
+    ///
+    /// * `height` - The height (number of rows) of each frame in the movie.
+    /// * `width` - The width (number of columns) of each frame in the movie.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new empty `Movie` instance with the specified dimensions.
     pub fn new(height: usize, width: usize) -> Self {
         Movie {
             height,
@@ -126,6 +243,20 @@ impl Movie {
         }
     }
 
+    /// Creates a new `Movie` instance with blank frames.
+    ///
+    /// This function initializes a new `Movie` instance with the given height and width, and populates it
+    /// with a specified number of blank frames (frames with all pixels set to spaces).
+    ///
+    /// # Arguments
+    ///
+    /// * `height` - The height (number of rows) of each frame in the movie.
+    /// * `width` - The width (number of columns) of each frame in the movie.
+    /// * `frame_amount` - The number of blank frames to add to the movie.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new `Movie` instance with blank frames and the specified dimensions.
     pub fn new_blank(height: usize, width: usize, frame_amount: usize) -> Self {
         let mut movie = Movie::new(height, width);
         let blank_frame = Frame::new(height, width);
@@ -136,19 +267,53 @@ impl Movie {
     }
 
     // Frame manipulation
+    /// Adds a frame to the movie.
+    ///
+    /// This function adds a new frame to the collection of frames in the movie.
+    ///
+    /// # Arguments
+    ///
+    /// * `frame` - A reference to the frame to be added to the movie.
     pub fn add_frame(&mut self, frame: &Frame) {
         self.frames.push(frame.clone());
     }
 
-    // Overide functions
-    pub fn override_with_movie(&mut self, movie: &Movie) {
-        self.override_with_movie_at_position(0, 0, movie)
+    /// Overlays the movie's frames with frames from another movie.
+    ///
+    /// This function takes a reference to another `Movie` instance and overlays the movie's frames with
+    /// frames from the provided movie. The operation starts at the top-left corner of the movie.
+    ///
+    /// # Arguments
+    ///
+    /// * `movie` - A reference to another `Movie` instance whose frames will overlay the current movie's frames.
+    ///
+    /// # Example
+    /// ```
+    /// "XXX".overlay_with_movie("O") == "OXX"
+    /// ```
+    pub fn overlay_with_movie(&mut self, movie: &Movie) {
+        self.overlay_with_movie_at_position(0, 0, movie)
     }
 
-    pub fn override_with_movie_at_position(&mut self, x: i32, y: i32, movie: &Movie) {
+    /// Overlay the frames of the current movie with the frames of another movie at the specified position (x, y).
+    ///
+    /// This method overlays the frames of the provided `movie` onto the frames of the current movie
+    /// starting from the given position (x, y). The overlay is performed sequentially for each frame
+    /// in the current movie, using frames from the provided `movie` in a looping manner.
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - The x-coordinate of the position where the overlay will start.
+    /// * `y` - The y-coordinate of the position where the overlay will start.
+    /// * `movie` - A reference to the `Movie` whose frames will be overlaid onto the current movie's frames.
+    /// # Example
+    /// ```
+    /// "XXX".overlay_with_movie_at_position(0,1," O") == "X O"
+    /// ```
+    pub fn overlay_with_movie_at_position(&mut self, x: i32, y: i32, movie: &Movie) {
         let mut i: u32 = 0;
         for frame in self.frames.iter_mut() {
-            let _ = frame.override_with_frame_at_position(x, y, &movie.frames[i as usize]);
+            let _ = frame.overlay_with_frame_at_position(x, y, &movie.frames[i as usize]);
             i += 1;
             if i >= movie.frames.len() as u32 {
                 i = 0;
@@ -156,7 +321,18 @@ impl Movie {
         }
     }
 
-    pub fn override_with_movie_at_position_in_range(
+    /// overlays specific frames in the movie with frames from another movie at positions within a range.
+    ///
+    /// This function takes a reference to another `Movie` instance and overlays specific frames in the movie's collection
+    /// with frames from the provided movie. The overlay positions are determined by the `range` parameter.
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - The X-coordinate of the starting position for the overlay.
+    /// * `y` - The Y-coordinate of the starting position for the overlay.
+    /// * `movie` - A reference to another `Movie` instance whose frames will overlay the current movie's frames.
+    /// * `range` - A range specifying the frame indexes to be overridden within the movie.
+    pub fn overlay_with_movie_at_position_in_range(
         &mut self,
         x: i32,
         y: i32,
@@ -168,7 +344,7 @@ impl Movie {
         let mut i: u32 = 0;
         for (frame_index, frame) in self.frames.iter_mut().enumerate() {
             if copied_frame_indexes.contains(&(frame_index as u32)) {
-                let _ = frame.override_with_frame_at_position(x, y, &movie.frames[i as usize]);
+                let _ = frame.overlay_with_frame_at_position(x, y, &movie.frames[i as usize]);
                 i += 1;
                 if i >= movie.frames.len() as u32 {
                     i = 0;
@@ -177,6 +353,14 @@ impl Movie {
         }
     }
 
+    /// Plays the movie by printing its frames in sequence.
+    ///
+    /// This function displays the frames of the movie on the console, simulating animation.
+    /// The console screen is cleared before printing each frame to achieve a smooth animation effect.
+    ///
+    /// # Arguments
+    ///
+    /// * `frames_per_second` - The number of frames to be displayed per second.
     pub fn play(&self, frames_per_second: u32) {
         for frame in self.frames.iter() {
             print!("\x1B[2J\x1B[1;1H");
@@ -186,8 +370,26 @@ impl Movie {
             ));
         }
     }
-}
 
-fn main() {
-    
+    /// Gets the width of the frames in the movie.
+    ///
+    /// This function returns the width (number of columns) of the frames in the movie.
+    ///
+    /// # Returns
+    ///
+    /// Returns the width of the frames in the movie.
+    fn get_width(&self) -> usize {
+        self.width
+    }
+
+    /// Gets the height of the frames in the movie.
+    ///
+    /// This function returns the height (number of rows) of the frames in the movie.
+    ///
+    /// # Returns
+    ///
+    /// Returns the height of the frames in the movie.
+    fn get_height(&self) -> usize {
+        self.height
+    }
 }
